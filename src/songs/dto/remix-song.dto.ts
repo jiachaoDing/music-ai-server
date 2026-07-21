ca2224fb0fbe3f9f10bd194076d8f1f358e4b6b8
郭娅芳 <13655072+guo-yafang513@user.noreply.gitee.com>
2026-07-21 09:35:50 +0800

    完善播放模式设置

--- src/songs/dto/remix-song.dto.ts
+++ src/songs/dto/remix-song.dto.ts
@@ -1,12 +1,15 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class RemixSongDto {
+  @ApiPropertyOptional({ description: '翻唱作品标题' })
+  title?: string;
+
   @ApiProperty({ description: '新风格' })
   style: string;
 
-  @ApiProperty({ description: '用户修改后的歌词', required: false })
+  @ApiPropertyOptional({ description: '用户修改后的歌词' })
   lyrics?: string;
 
-  @ApiProperty({ description: '风格描述' })
+  @ApiProperty({ description: '二创说明' })
   prompt: string;
 }
